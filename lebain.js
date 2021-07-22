09a594193e1651eaeb01138502442db97b3f4d82
root <root@PC-CLARA.localdomain>
2021-07-22 08:20:58 +0200

    delete-js

--- lebain.js
+++
@@ -1,7 +0,0 @@
-let title = document.querySelector(".gallery__item--1");
-let content = document.querySelector(".gallery__item--2");
-
-title.addEventListener("click", () => {
-    title.style.color = "blue";
-    content.style.visibility = "visible";
-})
