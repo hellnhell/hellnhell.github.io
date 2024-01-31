f3be666fb83d2c48def43e6d1881a37841b4ee0f
hellnhell <elenmafe8@gmail.com>
2024-01-31 20:28:48 +0100

    scroll-effect-dark-mode

--- tnoc/src/app/app.component.ts
+++ tnoc/src/app/app.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tnoc';
+  backgroundPositionX = 0;
+  isDarkMode = true;
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+  }
+
+  onActivate(event: any) {
+    if (event.scrollEvent) {
+      event.scrollEvent.subscribe((scrollPos: number) =>
+        this.onMainComponentScroll(scrollPos)
+      );
+    }
+    if (event.themeToggle) {
+      event.themeToggle.subscribe(() => {
+        this.toggleTheme();
+      });
+    }
+  }
+  onMainComponentScroll(scrollPos: number) {
+    const movementFactor = 0.02;
+    const movement = scrollPos * movementFactor;
+
+    const fogLayer1 = document.getElementById('fog-layer-1') as HTMLElement;
+    const fogLayer2 = document.getElementById('fog-layer-2') as HTMLElement;
+
+    if (fogLayer1 && fogLayer2) {
+      fogLayer1.style.transform = `translateX(-${movement}%)`;
+      fogLayer2.style.transform = `translateX(${movement - 50}%)`;
+    }
+  }
 }
--- tnoc/src/app/main-content/main-content.component.ts
+++ tnoc/src/app/main-content/main-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, OnInit, Output, ViewChild } from '@angular/core';
 import * as p5 from 'p5';
 
 @Component({
@@ -7,9 +7,99 @@ import * as p5 from 'p5';
   styleUrls: ['./main-content.component.scss']
 })
 export class MainContentComponent implements OnInit {
+  @Output() scrollEvent = new EventEmitter<number>();
+  @Output() themeToggle = new EventEmitter<void>();
+
+  @ViewChild('mainMenu') mainMenu!: ElementRef<HTMLDivElement>;
+
+  @HostListener('window:wheel', ['$event'])
+  onWindowScroll(event: WheelEvent) {
+    if (this.mainMenu) {
+      const currentScroll = this.mainMenu.nativeElement.scrollLeft;
+      const scrollAmount = event.deltaY;
+      this.mainMenu.nativeElement.scrollLeft = currentScroll + scrollAmount;
+      this.scrollEvent.emit(this.mainMenu.nativeElement.scrollLeft);
+    }
+  }
+
+  isDarkMode = true;
+  menus = [
+    {
+      title: 'Photography',
+      isActive: true,
+      items: [
+        {
+          title: '#treeoflife',
+          year: '2022',
+          dimensions: 'Φ720',
+          image: '/assets/imgs/bollers2.JPG',
+        },
+        {
+          title: '#landscape',
+          year: '2022',
+          dimensions: 'Φ720',
+          image: '/assets/imgs/bollers2.JPG',
+        },
+        {
+          title: '#treeoflife',
+          year: '2022',
+          dimensions: 'Φ720',
+          image: '/assets/imgs/bollers2.JPG',
+        },
+        {
+          title: '#landscape',
+          year: '2022',
+          dimensions: 'Φ720',
+          image: '/assets/imgs/bollers2.JPG',
+        },
+      ],
+    },
+    {
+      title: 'Video',
+      isActive: true,
+      items: [
+        {
+          title: '#treeoflife',
+          year: '2022',
+          dimensions: 'Φ720',
+          image: '/assets/imgs/bollers2.JPG',
+        },
+        {
+          title: '#landscape',
+          year: '2022',
+          dimensions: 'Φ720',
+          image: '/assets/imgs/bollers2.JPG',
+        },
+      ],
+    },
+    {
+      title: 'Coding projects',
+      isActive: true,
+      items: [
+        {
+          title: '#treeoflife',
+          year: '2022',
+          dimensions: 'Φ720',
+          image: '/assets/imgs/bollers2.JPG',
+        },
+        {
+          title: '#landscape',
+          year: '2022',
+          dimensions: 'Φ720',
+          image: '/assets/imgs/bollers2.JPG',
+        },
+      ],
+    },
+    // ... other menus
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }  
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    this.themeToggle.emit();
+  }
 }
